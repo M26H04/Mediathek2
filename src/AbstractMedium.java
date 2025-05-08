44a7f78a4110ccc1805e2ea5252c80ce5154bb03
M26H04 <mheift04@gmail.com>
2025-05-08 23:38:43 +0200

    neueAbstractclass

---
+++ src/AbstractMedium.java
@@ -0,0 +1,45 @@
+abstract class AbstractMedium implements Medium
+{
+    private String _titel;
+    private String _kommentar;
+
+    public AbstractMedium(String titel, String kommentar)
+    {
+        assert titel != null : "Vorbedingung verletzt: titel != null";
+        assert kommentar != null : "Vorbedingung verletzt: kommentar != null";
+        _titel = titel;
+        _kommentar = kommentar;
+    }
+
+    @Override
+    public String getTitel()
+    {
+        return _titel;
+    }
+
+    @Override
+    public void setTitel(String titel)
+    {
+        assert titel != null : "Vorbedingung verletzt: titel != null";
+        _titel = titel;
+    }
+
+    @Override
+    public String getKommentar()
+    {
+        return _kommentar;
+    }
+
+    @Override
+    public void setKommentar(String kommentar)
+    {
+        assert kommentar != null : "Vorbedingung verletzt: kommentar != null";
+        _kommentar = kommentar;
+    }
+
+    @Override
+    public abstract String getFormatiertenString();
+
+    @Override
+    public abstract String getMedienBezeichnung();
+}
